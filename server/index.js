5231c69527c4ac56db6046b680c92872e13eab07
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2025-07-16 03:20:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://msnnews.onrender.com',
+  credentials: true
+}));
 
 const newsRoutes = require('./routes/newsRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -16,7 +20,7 @@ app.use('/api/auth', authRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(5000, () => console.log('🚀 Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch(err => console.log('MongoDB Error:', err));
-
