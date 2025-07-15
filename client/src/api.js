4cd40660abe25c764d96292ca53fbdd35deaead4
Himanshu sharma <143833074+Himanshusharma71@users.noreply.github.com>
2025-07-16 02:34:33 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://msn-news-clone-2.onrender.com', // or your deployed URL
+  baseURL: 'http://localhost:5000/api', // or your deployed URL
 });
 
 // Attach token to every request
